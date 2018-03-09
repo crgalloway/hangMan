9c091e368c80de9590e448709fb4019e8fa8270a
crgalloway <crgalloway@gmail.com>
2018-03-09 12:21:39 -0800

    updated logic for wrong guesses

--- server.js
+++ server.js
@@ -21,6 +21,17 @@ var wordBank = ["test", "frogger", "dandelion"]
 function getRandomWord(wordBank){
 	return wordBank[Math.floor(Math.random()*wordBank.length)]
 }
+
+function wordSearch(guess,randWord){
+	var letterDict = {}
+	for(var i =0; i<randWord.length;i++){
+		if (guess == randWord[i]){
+			letterDict[i] = guess
+		}
+	}
+	return letterDict
+}
+
 var randWord = getRandomWord(wordBank)
 var count = 5
 
@@ -31,4 +42,21 @@ var io = require('socket.io').listen(server)
 
 io.sockets.on('connection', function(socket){
 	socket.emit('display_word', {word: randWord,tries:count})
+	socket.on('guess_made',function(guess){
+		if (count==0){
+			randWord = getRandomWord(wordBank)
+			console.log(randWord)
+			count = 5
+			io.emit('display_word', {word: randWord,tries:count})
+		}
+		var result = wordSearch(guess,randWord);
+		if (Object.keys(result).length == 0){
+			count--;
+			io.emit('display_word', {word: randWord,tries:count})
+		}
+		else{
+			io.emit('correct_guess',result)
+		}
+	})
+	
 });
